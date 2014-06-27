0b73574dc046bad9d13e22a5695d971e29b49087
Dmitry Gritsay <unseductable@gmail.com>
2014-06-27 09:14:22 +0300

    Add record quality checking to mapper

--- mapper/mapper.rs
+++ mapper/mapper.rs
@@ -1,12 +1,15 @@
 use std::io;
 
 fn main() {
+    let quality = ['0', '1', '4', '5', '9'];
+
     for line in io::stdin().lines() {
         let line = line.unwrap();
         let year = line.slice(15, 19);
         let temperature = line.slice(87, 92);
+        let q = line[92];
 
-        if temperature != "+9999" {
+        if temperature != "+9999" && quality.iter().any(|e| q == *e as u8) {
             println!("{}\t{}", year, temperature);
         }
     }
