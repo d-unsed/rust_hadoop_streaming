19041ebc9cf0bdfc9dba1842b2e95a90ba63de35
Dmitry Gritsay <unseductable@gmail.com>
2014-07-02 22:53:03 +0300

    Change temp_str variable name in reducer

--- src/reducer.rs
+++ src/reducer.rs
@@ -11,9 +11,9 @@ fn main() {
         let data: ~[&str] = line.split('\t').collect();
 
         match data.as_slice() {
-            [year, temp_str] => {
+            [year, temp] => {
                 let year = from_str::<int>(year).unwrap();
-                let temp = from_str::<int>(temp_str.trim_right()).unwrap();
+                let temp = from_str::<int>(temp.trim_right()).unwrap();
                 let cmp = |_: &int, t: &mut int| if temp > *t { *t = temp };
 
                 map.insert_or_update_with(year, temp, cmp);
