ba53c1b5948b796b542d73635275f99f5cb4b486
Dmitry Gritsay <unseductable@gmail.com>
2015-10-19 22:00:00 +0300

    Update mapper for Rust 1.3

--- src/mapper.rs
+++ src/mapper.rs
@@ -1,17 +1,19 @@
 use std::io;
+use std::io::BufRead;
 
 fn main() {
-    let quality = ['0', '1', '4', '5', '9'];
+    let reader = io::stdin();
 
-    for line in io::stdin().lines() {
-        let line = line.unwrap();
-        let l = line.as_slice();
+    let valid_quality = vec!['0', '1', '4', '5', '9'];
 
-        let year = l.slice(15, 19);
-        let temperature = l.slice(87, 92);
-        let q = l.char_at(92);
+    let data: Vec<(String, String, char)> = reader.lock().lines().map(|line| {
+        let line = line.unwrap().to_string();
 
-        if temperature != "+9999" && quality.iter().any(|e| q == *e) {
+        ((&line[15..19]).to_string(), (&line[87..92]).to_string(), line.chars().nth(92).unwrap())
+    }).collect();
+
+    for (year, temperature, quality) in data {
+        if temperature != "+9999" && valid_quality.contains(&quality) {
             println!("{}\t{}", year, temperature);
         }
     }
