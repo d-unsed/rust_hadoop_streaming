2e5320d713575065f0171001ef1902356820c953
Dmitry Gritsay <unseductable@gmail.com>
2015-10-20 18:55:54 +0300

    Update reducer for Rust 1.3

--- src/reducer.rs
+++ src/reducer.rs
@@ -1,26 +1,41 @@
-extern crate collections;
-
-use std::io;
 use std::collections::HashMap;
+use std::io;
+use std::io::BufRead;
+use std::string::ToString;
+
+// Remove leading `+`
+// Should be deleted in Rust 1.4
+fn remove_leading_plus(s: String) -> String {
+    (&s[1..]).to_string()
+}
 
 fn main() {
-    let mut map: HashMap<int, int> = HashMap::new();
+    let reader = io::stdin();
 
-    for line in io::stdin().lines() {
+    let mut hash_map: HashMap<i32, i32> = HashMap::new();
+
+    reader.lock().lines().map(|line| {
         let line = line.unwrap();
-        let data: Vec<&str> = line.as_slice().split('\t').collect();
+        let data: Vec<String> = line.split('\t').map(ToString::to_string).collect();
 
-        match data.as_slice() {
-            [year, temp] => {
-                let year = from_str::<int>(year).unwrap();
-                let temp = from_str::<int>(temp.trim_right()).unwrap();
-                let cmp = |_: &int, t: &mut int| if temp > *t { *t = temp };
+        (data[0].clone(), data[1].clone())
+    }).map(|(year, temperature)| {
+        let year = year.parse::<i32>().unwrap();
+        let temperature = temperature.parse::<i32>().unwrap_or_else(|_| {
+            remove_leading_plus(temperature).parse::<i32>().unwrap()
+        });
 
-                map.insert_or_update_with(year, temp, cmp);
-            },
-            _ => {}
-        }
+        (year, temperature)
+    }).scan(&mut hash_map, |hash_map, (year, temperature)| {
+        hash_map
+            .get_mut(&year)
+            .map(|e| if temperature > *e { *e = temperature })
+            .or_else(|| { hash_map.insert(year, temperature); Some(()) });
+
+            Some((year, temperature))
+    }).collect::<Vec<(i32, i32)>>();
+
+    for (year, temperature) in hash_map.iter() {
+        println!("{}\t{}", year, temperature);
     }
-
-    for (y, t) in map.iter() { println!("{}\t{}", y, t); }
 }
